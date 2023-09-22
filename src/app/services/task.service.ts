6f8ee2bc38136a91c537139abbc0cfde9aa1369f
Fausto Nunes <faustornunes@gmail.com>
2023-09-22 18:24:29 +0100

    Add json-server and mock tasks to db.json

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { TASKS } from '../mock-tasks';
+import { Task } from '../Task';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  constructor() {}
 
-  constructor() { }
+  getTasks(): Observable<Task[]> {
+    const tasks = of(TASKS);
+    return tasks;
+  }
 }
