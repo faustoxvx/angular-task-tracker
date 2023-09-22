f01151f4ac496d778e92ca9b6d5c7a646adaf578
Fausto Nunes <faustornunes@gmail.com>
2023-09-22 17:08:39 +0100

    Create Task interface

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -1,20 +1,20 @@
 export const TASKS = [
   {
     id: 1,
-    text: 'Doctors Appointment',
-    day: 'May 5th at 2:30pm',
+    text: "Mother-in-law's Birthday Lunch",
+    day: 'September 23rd at 1:30pm',
     reminder: true,
   },
   {
     id: 2,
-    text: 'Meeting at School',
-    day: 'May 6th at 1:30pm',
-    reminder: true,
+    text: 'Playing Dice Throne Adventures Campaign',
+    day: 'September 25th at 3:00pm',
+    reminder: false,
   },
   {
     id: 3,
     text: 'Food Shopping',
-    day: 'May 7th at 12:30pm',
+    day: 'September 25th at 6:30pm',
     reminder: false,
   },
 ];
